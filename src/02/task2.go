937a3f349b8ffbe6307b5569e0cdf8a0249fbfd9
Noah <noah@dreckbu.de>
2024-12-04 22:29:02 +0100

    Day 2 part 1

--- src/02/task2.go
+++ src/02/task2.go
@@ -11,6 +11,43 @@ func isSafeStep(a int, b int) bool {
 	return diff >= 1 && diff <= 3
 }
 
+func checkReportInc(report []int, isIncreasing bool) bool {
+	for i := 0; i < len(report)-1; i++ {
+		tokenA := report[i]
+		tokenB := report[i+1]
+
+		var isNotMonotonous bool
+
+		if isIncreasing {
+			isNotMonotonous = tokenB >= tokenA
+		} else {
+			isNotMonotonous = tokenA >= tokenB
+		}
+
+		if isNotMonotonous || !isSafeStep(tokenA, tokenB) {
+			return false
+		}
+	}
+
+	return true
+}
+
 func main() {
-	fmt.Println(isSafeStep(2, 4))
+	lines := common.ReadLinesFromFile("src/02/input.txt")
+
+	numSafe := 0
+
+	for _, line := range lines {
+		tokens := common.TokenizeLineAsInts(line)
+
+		isSafe := checkReportInc(tokens, true) || checkReportInc(tokens, false)
+
+		if isSafe {
+			numSafe += 1
+		}
+
+		fmt.Printf("%v [safe: %t]\n", tokens, isSafe)
+	}
+
+	fmt.Printf("%d reports are safe", numSafe)
 }
--- src/common/common.go
+++ src/common/common.go
@@ -1,9 +1,64 @@
 package common
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func Abs(x int) int {
 	if x < 0 {
 		return -x
 	} else {
 		return x
 	}
+}
+
+func TokenizeLine(line string) []string {
+	return strings.Fields(line)
+}
+
+func TokenizeLineAsInts(line string) []int {
+	tokens := TokenizeLine(line)
+
+	var out []int
+
+	for _, token := range tokens {
+		intToken, err := strconv.Atoi(token)
+
+		if err != nil {
+			panic(err)
+		}
+
+		out = append(out, intToken)
+	}
+
+	return out
+}
+
+func ReadLinesFromFile(filePath string) []string {
+	// open the file
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		fmt.Println("error opening file", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("error scanning file", err)
+	}
+
+	return lines
 }
